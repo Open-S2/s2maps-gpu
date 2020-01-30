443af76e0eef3ac49d97672dab2e82c648da4a10
CraigglesO <oconnorct1@gmail.com>
2020-01-29 23:00:40 -0500

    step 2

--- src/s2/shaders/ST2XYZ.glsl
+++ src/s2/shaders/ST2XYZ.glsl
@@ -1,6 +1,6 @@
 const float EXTENT = 4096.;
 
-uniform int uFXY[3]; // [face, x, y]
+uniform int uFST[5]; // [face, sLow, tLow, sHigh, tHigh]
 
 float STtoUV (float s) {
   // compressed VTs are extended, so we must squeeze them back to [0,1]
@@ -13,13 +13,15 @@ vec4 ST2XYZ (in vec2 st) { // x -> s, y -> t
   // prep xyz
   vec3 xyz;
   // convert to uv
-  vec2 uv = vec2(STtoUV(st.x), STtoUV(st.y)); // x -> u, y -> v
+  float s = STtoUV(st.x);
+  float t = STtoUV(st.y);
+  vec2 uv = vec2(s, t); // x -> u, y -> v
   // convert to xyz according to face
-  if (uFXY[0] == 0) xyz = vec3(uv.x, uv.y, 1);
-  else if (uFXY[0] == 1) xyz = vec3(1, uv.y, -uv.x);
-  else if (uFXY[0] == 2) xyz = vec3(-uv.y, 1, -uv.x);
-  else if (uFXY[0] == 3) xyz = vec3(-uv.y, -uv.x, -1);
-  else if (uFXY[0] == 4) xyz = vec3(-1, -uv.x, uv.y);
+  if (uFST[0] == 0) xyz = vec3(uv.x, uv.y, 1);
+  else if (uFST[0] == 1) xyz = vec3(1, uv.y, -uv.x);
+  else if (uFST[0] == 2) xyz = vec3(-uv.y, 1, -uv.x);
+  else if (uFST[0] == 3) xyz = vec3(-uv.y, -uv.x, -1);
+  else if (uFST[0] == 4) xyz = vec3(-1, -uv.x, uv.y);
   else xyz = vec3(uv.x, -1, uv.y);
   // normalize data
   xyz = normalize(xyz);
--- src/s2/shaders/fill.vertex.glsl
+++ src/s2/shaders/fill.vertex.glsl
@@ -1,25 +1,21 @@
 #version 300 es
-precision mediump float;
+precision highp float;
 
 #define EXTENT 4096.
 
 layout (location = 0) in vec2 aPos;
 layout (location = 5) in float aIndex;
 
+uniform mat4 uMatrix;
+
 uniform float uInputs[16];
 uniform float uLayerCode[256];
 uniform float uFeatureCode[128];
 
 @import ./decodeFeature;
+@import ./ST2XYZ;
 
 out vec4 color;
-
-vec2 decodePosition (in vec2 uv) {
-  return vec2(
-    (uv.x / EXTENT) * 2. - 1.,
-    (uv.y / EXTENT) * 2. - 1.
-  );
-}
 
 void main () {
   // prep layer index and feature index positions
@@ -27,8 +23,8 @@ void main () {
   int featureIndex = 0;
   // decode color
   color = decodeFeature(true, index, featureIndex);
-  // convert points to -1 to +1 xy space
-  vec2 pos = decodePosition(aPos);
+  // convert xy extent space to xyz
+  vec3 pos = ST2XYZ(aPos);
   // set position
-  gl_Position = vec4(pos, 0, 1);
+  gl_Position = uMatrix * vec4(pos, 0, 1);
 }
