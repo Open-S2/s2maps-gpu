0b4d80e56ddfc166b7ac63d34e5ef9822a278bd6
CraigglesO <oconnorct1@gmail.com>
2020-09-05 00:25:55 -0400

    width and height params

--- src/App/Home/index.js
+++ src/App/Home/index.js
@@ -27,9 +27,9 @@ function Home () {
           <Link to="/streets">
             <div className="link-container">
               <picture>
-                <source async loading="lazy" className="link-image" alt="streets" srcSet={StreetsWEBP} type="image/webp" />
-                <source async loading="lazy" className="link-image" alt="streets" srcSet={StreetsJPG} type="image/jpeg" />
-                <img async loading="lazy" className="link-image" alt="streets" src={LightJPG} />
+                <source async loading="lazy" width="640px" height="329px" alt="streets" srcSet={StreetsWEBP} type="image/webp" />
+                <source async loading="lazy" width="640px" height="329px" alt="streets" srcSet={StreetsJPG} type="image/jpeg" />
+                <img async loading="lazy" width="640px" height="329px" alt="streets" src={LightJPG} />
               </picture>
               <div className="link-name">Streets</div>
             </div>
@@ -38,9 +38,9 @@ function Home () {
           <Link to="/light">
             <div className="link-container">
               <picture>
-                <source async loading="lazy" className="link-image" alt="light" srcSet={LightWEBP} type="image/webp" />
-                <source async loading="lazy" className="link-image" alt="light" srcSet={LightJPG} type="image/jpeg" />
-                <img async loading="lazy" className="link-image" alt="light" src={LightJPG} />
+                <source async loading="lazy" width="640px" height="329px" alt="light" srcSet={LightWEBP} type="image/webp" />
+                <source async loading="lazy" width="640px" height="329px" alt="light" srcSet={LightJPG} type="image/jpeg" />
+                <img async loading="lazy" width="640px" height="329px" alt="light" src={LightJPG} />
               </picture>
               <div className="link-name">Light</div>
             </div>
@@ -48,9 +48,9 @@ function Home () {
 
           <Link to="/raster" className="link-container">
             <picture>
-              <source async loading="lazy" className="link-image" alt="raster" srcSet={RasterWEBP} type="image/webp" />
-              <source async loading="lazy" className="link-image" alt="raster" srcSet={RasterJPG} type="image/jpeg" />
-              <img async loading="lazy" className="link-image" alt="raster" src={RasterJPG} />
+              <source async loading="lazy" width="640px" height="329px" alt="raster" srcSet={RasterWEBP} type="image/webp" />
+              <source async loading="lazy" width="640px" height="329px" alt="raster" srcSet={RasterJPG} type="image/jpeg" />
+              <img async loading="lazy" width="640px" height="329px" alt="raster" src={RasterJPG} />
             </picture>
             <div className="link-name">Raster</div>
           </Link>
