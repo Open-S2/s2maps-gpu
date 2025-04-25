f4df9c5de9bdd8e723eb22974caa3fac04486f2b
Mr Martian <craiggles@s2maps.io>
2025-04-25 13:06:59 -0400

    migrate back to linux; fix lints

--- s2/source/tile.ts
+++ s2/source/tile.ts
@@ -342,7 +342,7 @@ class Tile<C extends SharedContext, F extends SharedFeatures, M extends SharedMa
         parent !== undefined &&
         // corner-case: empty data/missing tile -> flushes ALL layers,
         // but that layer MAY BE inverted so we don't kill it.
-        !(('invert' in layerGuide && layerGuide.invert === true) ?? false)
+        !(('invert' in layerGuide && layerGuide.invert) ?? false)
       )
         featureGuides.splice(i, 1);
     }
--- s2/workers/source.worker.ts
+++ s2/workers/source.worker.ts
@@ -436,7 +436,7 @@ export default class SourceWorker {
       };
       for (const source of Object.values(mapSources)) {
         if (sourceNames.length > 0 && !sourceNames.includes(source.name)) continue;
-        if (source.isTimeFormat === true) continue;
+        if (source.isTimeFormat) continue;
         await source.tileRequest(mapID, { ...tile }, flush);
       }
       postMessage(flush);
@@ -465,7 +465,7 @@ export default class SourceWorker {
         layersToBeLoaded: new Set<number>(),
       };
       for (const source of Object.values(mapSources)) {
-        if (source.isTimeFormat !== true || !sourceNames.includes(source.name)) continue;
+        if (!source.isTimeFormat || !sourceNames.includes(source.name)) continue;
         await source.tileRequest(mapID, { ...tile }, flush);
       }
       postMessage(flush);
